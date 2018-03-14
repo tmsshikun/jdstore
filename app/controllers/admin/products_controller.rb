711bb1ceee14e773a42d1522e424129c8ed24f71
tmsshikun <tmsshikun@gmail.com>
2018-03-14 09:58:52 +0800

    only admin can access backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,9 @@
 class Admin::ProductsController < ApplicationController
+
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
   def index
     @products = Product.all
   end
@@ -34,5 +39,11 @@ class Admin::ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not_admin!"
+    end
   end
 end
