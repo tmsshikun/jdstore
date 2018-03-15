28f07a7b16892b21688a200613e181e610f2f358
tmsshikun <tmsshikun@gmail.com>
2018-03-15 21:52:02 +0800

    implement email with bug

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.configure do
+  config.action_mailer.default_url_options = { :host =>  "localhost:3000" }
+end
---
+++ test/mailers/previews/ordre_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/ordre_mailer
+class OrdreMailerPreview < ActionMailer::Preview
+
+end
